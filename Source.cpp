d76ab40003f8a02ff382990ca872942b9a335451
Piotr Lawniczak <209775@student.pwr.edu.pl>
2015-06-01 21:13:28 +0200

    gotow

--- Source.cpp
+++ Source.cpp
@@ -3,9 +3,7 @@
 #include <conio.h>
 #include <time.h>
 
-
 //*** MAKRA ASEMBLEROWE ***
-
 //pierwszy odczyt czasu, przeniesienie m³odszej czêœci TSC
 //do rejestru który nie zostanie nadpisany w drugim etapie
 	#define RDTSCP1()	__asm rdtscp	\
@@ -24,55 +22,60 @@
 //oraz index (offset) wczytany do rejestru edi poprzedni¹ funkcj¹
 	#define Odczytaj(tab) __asm mov cl, byte ptr[edi + tab]
 
-//*** FUNKCJE DLA PREPAROWANIA SEKWENCJI ***
 
+//*** FUNKCJE DLA PREPAROWANIA SEKWENCJI ***
 //ustawia bie¿¹cy czas podawanemu parametrowi
 void startCzas(clock_t *startTime){
 	*startTime = clock();
 }
 
 //na podstawie aktualnego i przekazanego jako parametr
-//wczeœniejszego czasu - oblicza czas, jaki up³yn¹³
+//wczeœniejszego czasu - oblicza i zwraca czas, jaki up³yn¹³
 double stopCzas(clock_t *startTime){
 	return static_cast <double>(clock() - *startTime) / CLOCKS_PER_SEC;
 }
 
 //*** ZMIENNE GLOBALNE ***
+unsigned int odstep = 1;														//odstêp - wielkoœæ skoku pomiêdzy komórkami danych
+const unsigned int rozmiarAlokMega =  480;										//rozmiar zajmowanego przez program miejsca w pamiêci w MB
 
+//------------------------------------------
 const unsigned char rozmiarChara = sizeof(char);
-const unsigned int rozmiarAlokMega =  450;										//rozmiar w MB (max 1850)
 const unsigned int rozmiarAlokByte = rozmiarAlokMega * 1048576 / rozmiarChara;	//konwersja z MB na B
 unsigned char tablica[rozmiarAlokByte];											//rezerwacja pamiêci dla programu
-
-const int iloscElemDoOdczytu = 480000000;										//dla techniki preparowania sekwencji
+unsigned char tablica2[rozmiarAlokByte];											//rezerwacja pamiêci dla programu - druga tablica
+unsigned char tablica3[rozmiarAlokByte];
 FILE *plik;
 
 //*** MAIN ***
 int main(){
 	srand((size_t)time(NULL));							//reset generatora liczb pseudolosowych
-	DWORD a;											//zmienna przechowuj¹ca obliczon¹ iloœæ cykli CPU
-
+	unsigned int timer, wybor;
+	double zmierzonyCzas;
+	clock_t *czasStartu = new clock_t;
 
 	printf("Trwa inicjalizacja %d MB pamieci losowymi wartosciami (char = %d bajt)...\n", rozmiarAlokMega, rozmiarChara);
 	for (int i = 0; i < rozmiarAlokByte; i++)
-		tablica[i] = rand() % 256;						//zape³nianie tablicy losowymi wartoœciami z przedzia³u [0-255]
-
-	clock_t *czasStartu = new clock_t;
-	double zmierzonyCzas;
-	int wybor, odstep = 1;
+		tablica3[i] = tablica2[i] = tablica[i] = rand() % 256;						//zape³nianie tablicy losowymi wartoœciami z przedzia³u [0-255]
+	
 	printf("Wybierz technike pomiaru:\n1. Preparowanie sekwencji (pomiar masowy)\n2. Pomiar czasu pojedynczej instrukcji\n\nPodaj wybor: ");
 	scanf_s("%d", &wybor);
+
 while (true){
 	switch (wybor){
 	case 1:
+		unsigned int skok;
+		printf("Podaj odstep miedzy komorkami: ");
+		scanf_s("%d", &skok);
 		startCzas(czasStartu);
-		for (int i = 0; i < iloscElemDoOdczytu; i++){
+		for (unsigned int i = 0; i < rozmiarAlokByte; i+= skok){
 			IterToEDI();
 			Odczytaj(tablica);
+			Odczytaj(tablica2);
 		}
 		zmierzonyCzas = stopCzas(czasStartu);
-		printf("Czas odczytu %d elementow z pamieci wynosi: %.3f [s]\n", iloscElemDoOdczytu, zmierzonyCzas);
-		printf("Sredni czas odczytu dla jednej komorki danych wynosi: %.3f [ns]\n", 1000000000*zmierzonyCzas / iloscElemDoOdczytu);
+		printf("Czas odczytu %d elementow z pamieci wynosi: %.3f [s]\n", 2*rozmiarAlokByte/skok, zmierzonyCzas);
+		printf("Sredni czas odczytu dla jednej komorki danych wynosi: %.3f [ns]\n", 1000000000 * zmierzonyCzas/(2*rozmiarAlokByte/skok));
 		break;
 
 	case 2:
@@ -80,21 +83,21 @@ while (true){
 		plik = fopen("wyniki.txt", "w");
 		printf("Odstepy pomiedzy wczytywanymi danymi: %d bajt (%d char).\n", odstep*rozmiarChara, odstep);
 		RDTSCP1();
-		RDTSCP2(a);
-		printf("Ilosc cykli w przypadku braku instrukcji (nop) = %d\n\n", a);
+		RDTSCP2(timer);
+		printf("Ilosc cykli w przypadku braku instrukcji (nop) = %d\n\n", timer);
 
-		for (int i = 0; i < 2050 * odstep; i += odstep) {	//wykonanie 70 prób na kolejnych elementach tablicy
-			IterToEDI();
+		for (unsigned int i = 0; i < 2050; i += odstep) {
+			IterToEDI();								//wczytanie offsetu "i" do rej. %edi, aby nie mierzyæ kilkukrotnego dostêpu do pamieci
 			RDTSCP1();									//czas start
-			Odczytaj(tablica);							//wycztanie chara spod adresu "tablica" + "i" do rejestru cl
-			RDTSCP2(a);									//czas stop
-			printf("%.02d. odczyt = %d\n", i / odstep, a);
-			fprintf(plik, "%.02d. odczyt = %d\n", i / odstep, a);
+			Odczytaj(tablica);							//wycztanie chara spod adresu "tablica" + "%edi" do rejestru cl
+			RDTSCP2(timer);								//czas stop
+			fprintf(plik, "%.02d. odczyt = %d\n", i / odstep, timer); //zapis linii do pliku
 		}
+		printf("Zakonczono. Wyniki w pliku \"wyniki.txt\".\n");
 		fclose(plik);
 	}
 	if (13 == _getch())									//czeka na znak z klawiatury
-		odstep *= 2;									//jeœli enter, to podwaja odstêp
+		odstep *= 2;									//jeœli enter, to podwaja odstêp miêdzy komórkami
 }
 	return 0;
 }
